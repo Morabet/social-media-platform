d3cda1d41d51f070940cd084a5d982519184b0ca
Morabet <ismailelmorabet2@gmail.com>
2024-04-28 23:57:01 +0100

    Craete Top Bottom and LeftSideBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import Home from "./_root/pages/Home";
+
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 
 const App = () => {
   return (
@@ -19,6 +30,14 @@ const App = () => {
         <Route element={<RootLayout />}>
           {/* private routes */}
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
     </main>
--- frontend/src/_auth/forms/SigninForm.jsx
+++ frontend/src/_auth/forms/SigninForm.jsx
@@ -1,7 +1,102 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SigninValidation } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
 
 const SigninForm = () => {
-  return <div>SigninForm</div>;
+  const navigate = useNavigate();
+  const isLoader = false;
+  // 1. Define your form.
+  const form = useForm({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  // 2. Define a submit handler.
+  const onSubmit = (values) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    navigate("/");
+  };
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Log in to your account
+        </h2>
+        <p className="m text-light-3 small-medium md:base-regular mt-2">
+          Welcome back! Please enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoader ? (
+              <div className="flex-center gap-2">
+                <Loader />
+                Loading...
+              </div>
+            ) : (
+              "Sign in"
+            )}
+          </Button>
+          <p className="textsmaill-regular text-light-2 text-center mt-2">
+            Don't have an account?
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibol ml-1"
+            >
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
 };
 
 export default SigninForm;
